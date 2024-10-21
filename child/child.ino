034f4397822fe51245581dd4e095e9f84a0127b8
CrackedP0t <elainamartineau@gmail.com>
2024-10-21 17:03:36 -0600

    Add ArduinoJSON

--- child/child.ino
+++ child/child.ino
@@ -106,18 +106,26 @@ void loop() {
     Serial.println(dht22.getLastError());
   }
 
-  Serial.print("h=");Serial.print(h,1);Serial.print("\t");
-  Serial.print("t=");Serial.println(t,1);
+  JsonDocument doc;
+
+  doc["r"] = "ok";
+  doc["h"] = h;
+  doc["t"] = t;
+
+  serializeJson(doc, Serial);
+
+  Serial.print("h:");Serial.print(h,1);Serial.print(",");
+  Serial.print("t:");Serial.println(t,1);
 
   // Broadcast a message to all devices within the network
   char data[32];
   snprintf(data, sizeof(data), "Hello, World! #%lu", msg_count++);
 
-  Serial.printf("Broadcasting message: %s\n", data);
+  // Serial.printf("Broadcasting message: %s\n", data);
 
   if (!broadcast_peer.send_message((uint8_t *)data, sizeof(data))) {
     Serial.println("Failed to broadcast message");
   }
 
-  delay(500);
+  delay(1000);
 }
